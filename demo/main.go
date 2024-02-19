934d19421242b7b0de2f784b667c53eba6a98b1e
wwilkowski <wwilkowski97@gmail.com>
2024-02-19 11:08:55 +0100

    Rewrite based on cr, eliminate metadata file, move queryParams to body or mux.Vars, organize code,  add comments and error handling

--- demo/main.go
+++ demo/main.go
@@ -11,12 +11,13 @@ import (
 
 func main() {
 	fs := afero.NewOsFs()
-	uploader := chunkeduploader.NewChunkedUploaderService(fs)
+	uploader := chunkeduploader.NewChunkedUploaderService(fs, ".")
 
 	r := mux.NewRouter()
-	r.HandleFunc("/init", uploader.InitUploadHandler).Methods("POST")
-	r.HandleFunc("/upload", uploader.UploadChunkHandler).Methods("POST")
-	r.HandleFunc("/finish", uploader.FinishUploadHandler).Methods("POST")
+	r.HandleFunc("/init", uploader.CreateUploadHandler).Methods("POST")
+	r.HandleFunc("/upload/{upload_id}", uploader.UploadChunkHandler).Methods("POST")
+	r.HandleFunc("/finish/{upload_id}", uploader.FinishUploadHandler).Methods("POST")
+	r.HandleFunc("/rename/{upload_id}", uploader.RenameUploadedFileHandler).Methods("POST")
 
 	fmt.Println("Server is running on port 8080")
 	err := http.ListenAndServe(":8080", r)
--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module github.com/Craftserve/chunked-uploader
 go 1.20
 
 require (
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
