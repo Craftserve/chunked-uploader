775222a5ac8ac6b36668d7687c56cc7fee268718
wwilkowski <wwilkowski97@gmail.com>
2024-03-26 13:51:07 +0100

    Fix range

--- pkg/client/main.go
+++ pkg/client/main.go
@@ -85,7 +85,7 @@ func (c *Client) Upload(file os.File, chunkSize int64) (path string, err error) 
 	c.Endpoints.Upload = strings.Replace(c.Endpoints.Upload, "{upload_id}", upload_id, 1)
 	c.Endpoints.Finish = strings.Replace(c.Endpoints.Finish, "{upload_id}", upload_id, 1)
 
-	noOfChunks := math.Ceil(float64(fileInfo.Size() / int64(chunkSize)))
+	noOfChunks := math.Ceil(float64(fileInfo.Size() / chunkSize))
 
 	var wg sync.WaitGroup
 	errc := make(chan error, 10)
@@ -127,7 +127,7 @@ func (c *Client) Upload(file os.File, chunkSize int64) (path string, err error) 
 			}
 
 			req.Header.Set("Content-Type", writer.FormDataContentType())
-			req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", start, end))
+			req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", start, end-1))
 			for _, cookie := range c.Config.Cookies {
 				req.AddCookie(cookie)
 			}
